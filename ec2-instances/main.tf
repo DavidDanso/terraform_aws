13b92b05945d2fefb4c11bbaf7387a6f7ff25257
DavidDanso <desiingerboy@icloud.com>
2024-03-28 13:48:37 +0000

    added ingress and egress rules

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -14,11 +14,32 @@ provider "aws" {
   region = "us-west-2" # Specify the region for the AWS provider
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+# Resource for the security group
+resource "aws_security_group" "http_server_sg" {
+  name        = "http_server_sg"
+  vpc_id      = "vpc-0b34c76d8b6819e15"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
+  # Restrict inbound HTTP access (port 80) to a specific IP address or security group (replace with your desired allowed source)
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  # Replace with specific source
+  }
+
+  # ingress rule for SSH access (port 22) with similar restrictions
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  # Replace with specific source for SSH
+  }
+
+  # Egress rule (consider restricting based on your needs)
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]  # You might want to restrict outbound traffic based on your application's requirements
   }
 }
