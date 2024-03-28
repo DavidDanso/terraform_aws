29e12dfc3e2a69ee38ced1707ff521b4bf16436b
DavidDanso <desiingerboy@icloud.com>
2024-03-28 15:51:16 +0000

    provisioning ec2 instace with terraform

--- ec2-instances/main.tf
+++ ec2-instances/main.tf
@@ -16,30 +16,30 @@ provider "aws" {
 
 # Resource for the security group
 resource "aws_security_group" "http_server_sg" {
-  name        = "http_server_sg"
-  vpc_id      = "vpc-0b34c76d8b6819e15"
+  name   = "http_server_sg"
+  vpc_id = "vpc-0b34c76d8b6819e15"
 
   # Restrict inbound HTTP access (port 80) to a specific IP address or security group (replace with your desired allowed source)
   ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]  # Replace with specific source
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with specific source
   }
 
   # ingress rule for SSH access (port 22) with similar restrictions
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]  # Replace with specific source for SSH
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with specific source for SSH
   }
 
   # Egress rule (consider restricting based on your needs)
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]  # You might want to restrict outbound traffic based on your application's requirements
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"] # You might want to restrict outbound traffic based on your application's requirements
   }
 }
